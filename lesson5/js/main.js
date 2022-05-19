86d983629089cafa88835495e0cb3e1841d31385
David F <flo22001@byui.edu>
2022-05-18 23:25:33 -0400

    DOM Manipulation activity

---
+++ lesson5/js/main.js
@@ -0,0 +1,32 @@
+const favchapInput =  document.getElementById("favchap");
+const addChapterBtn  = document.getElementById("addChapterBtn");
+const listEl =  document.querySelector(".list");
+
+addChapterBtn.addEventListener("click", ()=>{
+    let enteredValue = favchapInput.value;
+    //checking if textbox is empty
+    if(enteredValue.trim() != ""){
+        // new list item
+        const newListItem = document.createElement("li");
+        newListItem.textContent = enteredValue;
+        // new button
+        const newDeleteBtn = document.createElement("button");
+        newDeleteBtn.textContent = '❌';
+        // button delete event handler
+        newDeleteBtn.addEventListener("click", ()=>{
+            listEl.removeChild(newListItem);
+        });
+        // adding button to the list item
+        newListItem.appendChild(newDeleteBtn);
+        
+        // adding list item to the list
+        listEl.appendChild(newListItem);
+
+        //clearing textbox
+        favchapInput.value = "";
+    }
+
+    // sents focus back to textbox
+    favchapInput.focus();
+
+});
