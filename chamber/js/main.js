dccaaad4a29079e058a532c55b72799d35b5f917
David F <flo22001@byui.edu>
2022-05-21 13:01:33 -0400

    style fixes and added meeting banner

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -13,10 +13,17 @@ window.onresize = () => {
 };
 
 // date fields
-let dateField = document. querySelector("#last-updated");
-let yearField = document. querySelector("#current-year");
+const dateField = document. querySelector("#last-updated");
+const yearField = document. querySelector("#current-year");
+const currentDateDiv = document.getElementById("currentdate");
 //current date
 let currentDate = new Date();
 //setting date values
 dateField.innerHTML = document.lastModified;
 yearField.innerHTML = currentDate.getFullYear();
+currentDateDiv.textContent = currentDate.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}); // Formatting neeeded
+// annoucement (meeting)
+const announcement = document.getElementById("announcement");
+const dayNumber = currentDate.getDay()
+// 1= Mon, 2=Tue
+if (dayNumber == 1 || dayNumber == 2) announcement.style.display = 'block';
