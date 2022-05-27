aeaceceaacd7b7e04bd3a90b01d080c92dd94f34
David F <flo22001@byui.edu>
2022-05-27 15:12:36 -0400

    Week 5 activities

---
+++ chamber/js/windchill.js
@@ -0,0 +1,14 @@
+const weather_degrees = document.getElementById("weather_degrees");
+const weather_windspeed = document.getElementById("weather_windspeed");
+const weather_windchill = document.getElementById("weather_windchill");
+
+let temp = parseFloat(weather_degrees.textContent);
+let wind_speed = parseFloat(weather_windspeed.textContent);
+let wind_chill = 'N/A';
+
+if(temp <=50 && wind_speed > 3){
+    let chill_value = 35.74 + (0.6215 * temp)- 35.75 * (wind_speed ** 0.16) + 0.4275 * temp * (wind_speed ** 0.16);
+    wind_chill = chill_value.toFixed(1) + ' ℉';
+}
+//updating value on screen
+weather_windchill.textContent = wind_chill;
