0e2de764895816b6757c20f6d6753ed175b54d39
David F <flo22001@byui.edu>
2022-06-24 22:30:54 -0400

    added weather API call

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,35 @@
-const weather_degrees = document.getElementById("weather_degrees");
-const weather_windspeed = document.getElementById("weather_windspeed");
-const weather_windchill = document.getElementById("weather_windchill");
+const weatherDegrees = document.getElementById("weather_degrees");
+const weatherWindSpeed = document.getElementById("weather_windspeed");
+const weatherWindChill = document.getElementById("weather_windchill");
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Buenos+Aires,AR&units=imperial&appid=";
 
-if(weather_degrees && weather_windchill && weather_windchill){
-    let temp = parseFloat(weather_degrees.textContent);
-    let wind_speed = parseFloat(weather_windspeed.textContent);
+// if Weather elements are on the page then gets temperature
+if(weatherDegrees && weatherWindSpeed && weatherWindChill) {
+  apiFetch();
+}
+
+function displayResults(weatherData) {
+    const tempDesc = document.getElementById("temp_desc");
+    const weatherIcon = document.getElementById("weather_icon");
+    const humidity = document.getElementById("weather_humidity");
+    // icon properties
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    // setting icon
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('srcset', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    // temperature description
+    tempDesc.textContent = desc;
+    // temperature
+    weatherDegrees.textContent = weatherData.main.temp.toFixed(0);
+    // wind speed
+    weatherWindSpeed.textContent = weatherData.wind.speed.toFixed(1);
+    humidity.textContent = weatherData.main.humidity.toFixed(0);
+    
+    // windchill calculation
+    let temp = weatherData.main.temp; //parseFloat(weather_degrees.textContent);
+    let wind_speed = weatherData.wind.speed; //parseFloat(weatherWindSpeed.textContent);
     let wind_chill = 'N/A';
     
     if(temp <=50 && wind_speed > 3){
@@ -12,5 +37,21 @@ if(weather_degrees && weather_windchill && weather_windchill){
         wind_chill = chill_value.toFixed(1) + ' ℉';
     }
     //updating value on screen
-    weather_windchill.textContent = wind_chill;
+    weatherWindChill.textContent = wind_chill;
 }
+
+async function apiFetch() {
+    try {
+      const ak = "e3cf141a197ad7d8f55f34115bd5bcd5";
+      const response = await fetch(url + ak);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
