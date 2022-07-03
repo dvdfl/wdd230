d35c794bb6c5c7f7c9ba30f10d78ca0e693dc832
David F <flo22001@byui.edu>
2022-07-02 22:36:23 -0400

    added contact page

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -49,3 +49,34 @@ const dateFormField = document.getElementById("date_time");
 if (dateFormField) {
     dateFormField.value = currentDate;
 }
+
+
+// spotlights
+(function(){
+    const container = document.getElementById("spotlights");
+    if(container) {
+        fetch("js/companies.txt").then(function (response) {
+            return response.json();
+          }).then(function(data) {
+            const companies = [];
+            //filtering silver and gold
+            silverGoldCompanies = data.companies.filter(c => c.membership == "silver" || c.membership == "gold");
+            // ramdomlyselecting companies using Math.random(); 
+            console.log(silverGoldCompanies.length)
+            companies.push(silverGoldCompanies[Math.floor(Math.random() * silverGoldCompanies.length)]);
+            companies.push(silverGoldCompanies[Math.floor(Math.random() * silverGoldCompanies.length)]);
+            companies.push(silverGoldCompanies[Math.floor(Math.random() * silverGoldCompanies.length)]);
+            
+            companies.forEach(company => {
+                const section = document.createElement("section");
+                section.innerHTML = `<h5>${company.name} <span class="membership_${company.membership}" title="${company.membership} member">★</span></h5>`;
+                section.innerHTML += `<img src="${company.logo}" alt="${company.name} Logo">`;
+                section.innerHTML += `<p>${company.description}</p>`;
+                section.innerHTML += `<div class="spotlight-footer"><tel>${company.phone}</tel></div>`;
+                container.appendChild(section);
+            });
+            
+          });
+    }
+
+})();
